9de0462b84a8731a234532c4da2bc0bcb7bcfe61
Dust765 <dust765@protonmail.com>
2022-08-12 11:34:36 +0200

    VISUAL HELPERS

--- src/Game/GameObjects/Views/StaticView.cs
+++ src/Game/GameObjects/Views/StaticView.cs
@@ -33,7 +33,9 @@
 using ClassicUO.Configuration;
 using ClassicUO.Game.Data;
 // ## BEGIN - END ## // ART / HUE CHANGES
+// ## BEGIN - END ## // VISUAL HELPERS
 using ClassicUO.Dust765.Dust765;
+// ## BEGIN - END ## // VISUAL HELPERS
 // ## BEGIN - END ## // ART / HUE CHANGES
 using ClassicUO.Game.Scenes;
 using ClassicUO.IO;
@@ -92,6 +94,34 @@ namespace ClassicUO.Game.GameObjects
 
             Vector3 hueVec = ShaderHueTranslator.GetHueVector(hue, partial, AlphaHue / 255f);
 
+            // ## BEGIN - END ## // VISUAL HELPERS
+            if (ProfileManager.CurrentProfile.HighlightTileAtRange && Distance == ProfileManager.CurrentProfile.HighlightTileAtRangeRange)
+            {
+                hueVec.X = ProfileManager.CurrentProfile.HighlightTileRangeHue;
+                hueVec.Y = 1;
+            }
+            if (ProfileManager.CurrentProfile.HighlightTileAtRangeSpell)
+            {
+                if (GameCursor._spellTime >= 1 && Distance == ProfileManager.CurrentProfile.HighlightTileAtRangeRangeSpell)
+                {
+                    hueVec.X = ProfileManager.CurrentProfile.HighlightTileRangeHueSpell;
+                    hueVec.Y = 1;
+                }
+            }
+            if (ProfileManager.CurrentProfile.PreviewFields)
+            {
+                if (CombatCollection.StaticFieldPreview(this))
+                {
+                    hueVec.X = 0x0040;
+                    hueVec.Y = 1;
+                }
+                if (SelectedObject.Object == this)
+                {
+                    hueVec.X = 0x0023;
+                    hueVec.Y = 1;
+                }
+            }
+            // ## BEGIN - END ## // VISUAL HELPERS
             // ## BEGIN - END ## // ART / HUE CHANGES
             /*
             bool isTree = StaticFilters.IsTree(graphic, out _);
