778f4e11743ccddc564399773f1796c600b8951e
Dust765 <dust765@protonmail.com>
2022-08-15 13:49:46 +0200

    BUFFBAR/UCCSETTINGS

--- src/Game/Scenes/GameScene.cs
+++ src/Game/Scenes/GameScene.cs
@@ -250,6 +250,17 @@ namespace ClassicUO.Game.Scenes
 
             }
             // ## BEGIN - END ## // AUTOLOOT
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
+            if (ProfileManager.CurrentProfile.UOClassicCombatBuffbar)
+            {
+                UIManager.Add(new UOClassicCombatBuffbar
+                {
+                    X = ProfileManager.CurrentProfile.UOClassicCombatBuffbarLocation.X,
+                    Y = ProfileManager.CurrentProfile.UOClassicCombatBuffbarLocation.Y
+                });
+
+            }
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
 
             CircleOfTransparency.Create(ProfileManager.CurrentProfile.CircleOfTransparencyRadius);
             Plugin.OnConnected();
@@ -361,6 +372,12 @@ namespace ClassicUO.Game.Scenes
             // ## BEGIN - END ## // UI/GUMPS
             World.Player?.BandageTimer.OnMessage(text, hue, name, e.IsUnicode);
             // ## BEGIN - END ## // UI/GUMPS
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
+            if (ProfileManager.CurrentProfile.UOClassicCombatBuffbar)
+            {
+                World.ClilocTriggers.OnMessage(text, hue, name, e.IsUnicode);
+            }
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
 
             if (!string.IsNullOrEmpty(text))
             {
