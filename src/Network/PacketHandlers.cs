9dd3e495c98e6742560e5567dd7a7349d4c0cc35
Dust765 <dust765@protonmail.com>
2022-08-15 09:07:53 +0200

    MACROS

--- src/Network/PacketHandlers.cs
+++ src/Network/PacketHandlers.cs
@@ -2242,6 +2242,11 @@ namespace ClassicUO.Network
                 forward,
                 true
             );
+
+            // ## BEGIN - END ## // MACROS
+            if (mobile == World.Player)
+                World.AnimationTriggers.OnOwnCharacterAnimation(action);
+            // ## BEGIN - END ## // MACROS
         }
 
         private static void GraphicEffect(ref StackDataReader p)
@@ -5465,6 +5470,11 @@ namespace ClassicUO.Network
             byte group = Mobile.GetObjectNewAnimation(mobile, type, action, mode);
 
             mobile.SetAnimation(group, repeatCount: 1, repeat: (type == 1 || type == 2) && mobile.Graphic == 0x0015, forward: true, fromServer: true);
+
+            // ## BEGIN - END ## // MACROS
+            if (mobile == World.Player && type == 0)
+                World.AnimationTriggers.OnOwnCharacterAnimationNew(action, type);
+            // ## BEGIN - END ## // MACROS
         }
 
         private static void KREncryptionResponse(ref StackDataReader p)
