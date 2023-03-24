b46aaca10047b33fd1f90787801b5aaa15056f8e
dust765@protonmail.com <dust765@protonmail.com>
2023-03-24 15:33:34 +0100

    MISC

--- src/ClassicUO.Client/Game/UI/Gumps/HealthBarGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/HealthBarGump.cs
@@ -71,6 +71,10 @@ namespace ClassicUO.Game.UI.Gumps
             _name = entity.Name;
             _isDead = entity is Mobile mm && mm.IsDead;
 
+            // ## BEGIN - END ## // MISC
+            LocalEntity = entity;
+            // ## BEGIN - END ## // MISC
+
             BuildGump();
         }
 
@@ -174,13 +178,19 @@ namespace ClassicUO.Game.UI.Gumps
                 TargetManager.Target(LocalSerial);
                 Mouse.LastLeftButtonClickTime = 0;
             }
-            else if (_canChangeName && !_targetBroke)
+            // ## BEGIN - END ## // MISC
+            //else if (_canChangeName && !_targetBroke)
+            // ## BEGIN - END ## // MISC
+            else if (_canChangeName)
+            // ## BEGIN - END ## // MISC
             {
                 _textBox.IsEditable = true;
                 _textBox.SetKeyboardFocus();
             }
 
-            _targetBroke = false;
+            // ## BEGIN - END ## // MISC
+            //_targetBroke = false;
+            // ## BEGIN - END ## // MISC
         }
 
         protected static int CalculatePercents(int max, int current, int maxValue)
@@ -224,8 +234,20 @@ namespace ClassicUO.Game.UI.Gumps
 
             if (TargetManager.IsTargeting)
             {
-                _targetBroke = true;
+                // ## BEGIN - END ## // MISC
+                //_targetBroke = true;
+                // ## BEGIN - END ## // MISC
                 TargetManager.Target(LocalSerial);
+                // ## BEGIN - END ## // MISC
+                Entity ent = World.Get(LocalSerial);
+                if (ent == null)
+                {
+                    TargetManager.LastTargetInfo.Serial = LocalEntity.Serial;
+                    GameActions.Print($"Changing last target to {LocalEntity.Name}");
+                    GameActions.Print(World.Player, $"Target: {LocalEntity.Name}");
+                    TargetManager.CancelTarget();
+                }
+                // ## BEGIN - END ## // MISC
                 Mouse.LastLeftButtonClickTime = 0;
             }
             else if (_canChangeName)
