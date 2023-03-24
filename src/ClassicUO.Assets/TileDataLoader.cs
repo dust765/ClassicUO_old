ebd24f6b2f793239513573191bfa528109474aca
dust765@protonmail.com <dust765@protonmail.com>
2023-03-24 15:33:34 +0100

    MISC

--- src/ClassicUO.Assets/TileDataLoader.cs
+++ src/ClassicUO.Assets/TileDataLoader.cs
@@ -353,6 +353,10 @@ namespace ClassicUO.Assets
             LightIndex = lightIndex;
             Height = height;
             Name = name;
+
+            // ## BEGIN - END ## // MISC
+            IsImpassable = (Flags & TileFlag.Impassable) != 0;
+            // ## BEGIN - END ## // MISC
         }
 
         public TileFlag Flags;
@@ -367,7 +371,11 @@ namespace ClassicUO.Assets
 
         public bool IsAnimated => (Flags & TileFlag.Animation) != 0;
         public bool IsBridge => (Flags & TileFlag.Bridge) != 0;
-        public bool IsImpassable => (Flags & TileFlag.Impassable) != 0;
+        // ## BEGIN - END ## // MISC
+        //public bool IsImpassable => (Flags & TileFlag.Impassable) != 0;
+        // ## BEGIN - END ## // MISC
+        public bool IsImpassable;
+        // ## BEGIN - END ## // MISC
         public bool IsSurface => (Flags & TileFlag.Surface) != 0;
         public bool IsWearable => (Flags & TileFlag.Wearable) != 0;
         public bool IsInternal => (Flags & TileFlag.Internal) != 0;
