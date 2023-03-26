f9cf5cd045f470c11155f27dbd00e24a517946b8
dust765@protonmail.com <dust765@protonmail.com>
2023-03-26 22:28:57 +0200

    BUFFBAR/UCCSETTINGS

--- src/ClassicUO.Client/Dust765/Dust765/AnimationTriggers.cs
+++ src/ClassicUO.Client/Dust765/Dust765/AnimationTriggers.cs
@@ -40,6 +40,10 @@ namespace ClassicUO.Dust765.Dust765
                 // ## BEGIN - END ## // MACROS
                 CombatCollection._HarmOnSwingTrigger = true;
                 // ## BEGIN - END ## // MACROS
+                // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
+                UOClassicCombatBuffbar UOClassicCombatBuffbar = UIManager.GetGump<UOClassicCombatBuffbar>();
+                UOClassicCombatBuffbar?.ClilocTriggerSwing();
+                // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
             }
             return;
         }
@@ -48,6 +52,10 @@ namespace ClassicUO.Dust765.Dust765
             // ## BEGIN - END ## // MACROS
             CombatCollection._HarmOnSwingTrigger = true;
             // ## BEGIN - END ## // MACROS
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
+            UOClassicCombatBuffbar UOClassicCombatBuffbar = UIManager.GetGump<UOClassicCombatBuffbar>();
+            UOClassicCombatBuffbar?.ClilocTriggerSwing();
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
 
             return;
         }
--- src/ClassicUO.Client/Network/PacketHandlers.cs
+++ src/ClassicUO.Client/Network/PacketHandlers.cs
@@ -4745,6 +4745,9 @@ namespace ClassicUO.Network
                 }
             }
             // ## BEGIN - END ## // AUTOLOOT
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
+            World.ClilocTriggers.OnCliloc(cliloc);
+            // ## BEGIN - END ## // BUFFBAR/UCCSETTINGS
 
             if (cliloc == 1008092 || cliloc == 1005445) // value for "You notify them you don't want to join the party" || "You have been added to the party"
             {
