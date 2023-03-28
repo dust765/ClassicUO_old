a83e2179d547cb4ed1db30edb7c20e69f13636cb
dust765@protonmail.com <dust765@protonmail.com>
2023-03-28 09:35:45 +0200

    MISC3 SHOWALLLAYERS (by Mark)

--- src/ClassicUO.Client/Game/GameObjects/Views/MobileView.cs
+++ src/ClassicUO.Client/Game/GameObjects/Views/MobileView.cs
@@ -1163,6 +1163,13 @@ namespace ClassicUO.Game.GameObjects
                 return false;
             }
 
+            // ## BEGIN - END ## // MISC3 SHOWALLLAYERS
+            if (ProfileManager.CurrentProfile != null && ProfileManager.CurrentProfile.ShowAllLayers)
+            {
+                return false;
+            }
+            // ## BEGIN - END ## // MISC3 SHOWALLLAYERS
+
             switch (layer)
             {
                 case Layer.Shoes:
