df53222982ab855ae1a5644d94d35c12f73d1942
dust765@protonmail.com <dust765@protonmail.com>
2023-03-28 08:31:03 +0200

    MODERNCOOLDOWNBAR

--- src/ClassicUO.Client/Configuration/Profile.cs
+++ src/ClassicUO.Client/Configuration/Profile.cs
@@ -41,6 +41,9 @@ using System.Xml;
 // ## BEGIN - END ## // MACROS
 using ClassicUO.Dust765.External;
 // ## BEGIN - END ## // MACROS
+// ## BEGIN - END ## // MODERNCOOLDOWNBAR
+using ClassicUO.Dust765.Dust765;
+// ## BEGIN - END ## // MODERNCOOLDOWNBAR
 using ClassicUO.Configuration.Json;
 using ClassicUO.Game;
 using ClassicUO.Game.GameObjects;
@@ -567,6 +570,9 @@ namespace ClassicUO.Configuration
         // ## BEGIN - END ## // STATUSGUMP
         public bool UseRazorEnhStatusGump { get; set; } = false;
         // ## BEGIN - END ## // STATUSGUMP
+        // ## BEGIN - END ## // MODERNCOOLDOWNBAR
+        public bool ModernCooldwonBar_locked { get; set; } = false;
+        // ## BEGIN - END ## // MODERNCOOLDOWNBAR
         // ## BEGIN - END ## // BASICSETUP
 
         public bool ShowJournalClient { get; set; } = true;
@@ -786,6 +792,28 @@ namespace ClassicUO.Configuration
                                     gump = new BuffGump();
 
                                     break;
+
+                                // ## BEGIN - END ## // MODERNCOOLDOWNBAR
+                                case GumpType.ECBuffGump:
+                                    gump = new ECBuffGump();
+
+                                    break;
+
+                                case GumpType.ECDebuffGump:
+                                    gump = new ECDebuffGump();
+
+                                    break;
+
+                                case GumpType.ECStateGump:
+                                    gump = new ECStateGump();
+
+                                    break;
+
+                                case GumpType.ModernCooldownBar:
+                                    gump = new ModernCooldownBar();
+
+                                    break;
+                                // ## BEGIN - END ## // MODERNCOOLDOWNBAR
 
                                 case GumpType.Container:
                                     gump = new ContainerGump();
